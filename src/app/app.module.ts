94b4fbcca3e8b8cdfed9cfdf4559525a25433147
Somanathan1987 <somubtech@gmail.com>
2019-11-13 18:47:22 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,6 @@ import { ProductListComponent } from './product-list/product-list.component';
 })
 export class AppModule { }
 
-
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
